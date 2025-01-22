ac0a3473027f84abdccb7528bdd9a91b0f0e339f
VictorTMendes <victortm2508@gmail.com>
2025-01-22 00:10:54 -0300

    update initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        Conta conta = new Conta();
+
+
+
+        scanner.close();
     }
+}
+
+class Conta{
+    private int id;
+    private String nome;
+    private double saldo;
+
+    public void realizarSaque(double saldo){
+
+    }
+
+    public void realizarDepósito(double saldo){
+
+    }
+
+    public double consultarSaldo(double saldo){
+        return saldo;
+    }
+
 }
